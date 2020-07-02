6a1ae1c368a42ffb2e2e5e53361d302e5ef27b9c
Christoph.Lauper <christoph.lauper@digitecgalaxus.ch>
2020-07-02 11:28:13 +0200

    return stopwords

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import {IKeyword} from '../types'
-import {callAllTrends} from './callGoogleApi'
-import {getSynonyms} from './callOpenThesaurusApi'
-import {IApiResponse, IApiResponseElement} from './IApiResponse'
-import {ISynonyms} from './ISynonyms'
-import {initKeywords, tokenizeAndSanitize} from './keywords'
-import {stopwordsDe} from './stopwords-de'
+import { IKeyword } from '../types'
+import { callAllTrends } from './callGoogleApi'
+import { getSynonyms } from './callOpenThesaurusApi'
+import { IApiResponse, IApiResponseElement } from './IApiResponse'
+import { ISynonyms } from './ISynonyms'
+import { initKeywords, tokenizeAndSanitize } from './keywords'
+import { stopwordsDe } from './stopwords-de'
 import express = require('express')
 
 const port = 8080
@@ -36,36 +36,41 @@ const getTrends = async (input: string): Promise<IApiResponse> => {
   for (const synonym of synonyms) {
     const synonymTrends = await callAllTrends(synonym.synonyms)
     for (const syn of synonym.synonyms) {
-      syn.normalizedTrend = synonymTrends.filter(st => st.keyword === syn.keyword)[0].normalizedTrend
+      syn.normalizedTrend = synonymTrends.filter(
+        (st) => st.keyword === syn.keyword
+      )[0].normalizedTrend
     }
   }
 
   return getApiResponse(sourceTrends, synonyms)
 }
 
-const getApiResponse = (sourceTrends: IKeyword[], synonyms: ISynonyms[]): IApiResponse => {
+const getApiResponse = (
+  sourceTrends: IKeyword[],
+  synonyms: ISynonyms[]
+): IApiResponse => {
   const responseElements = new Array<IApiResponseElement>()
-
-  sourceTrends.forEach(sourceTrend => {
+  console.log(sourceTrends)
+  sourceTrends.forEach((sourceTrend) => {
     let responseElement
     if (stopwordsDe.includes(sourceTrend.keyword.toLowerCase())) {
       responseElement = {
         keyword: sourceTrend.keyword,
-        synonyms: []
+        synonyms: [],
       }
     } else {
-    responseElement = {
-      keyWord: sourceTrend.keyword,
-      synonyms: synonyms
-        .filter(syn => syn.word === sourceTrend.keyword)[0]
-        .synonyms
-        .filter(syn => syn.normalizedTrend > 0)
+      responseElement = {
+        keyWord: sourceTrend.keyword,
+        synonyms: synonyms.filter((syn) => syn.word === sourceTrend.keyword)[0]
+          .synonyms,
+        //.filter(syn => syn.normalizedTrend > 0)
+      }
     }
     responseElements.push(responseElement)
-  }})
+  })
 
   return {
-    responseElements
+    responseElements,
   }
 }
 

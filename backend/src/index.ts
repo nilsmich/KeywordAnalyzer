f37856753da70da052e68cb8e863e6c9497d023e
Christoph.Lauper <christoph.lauper@digitecgalaxus.ch>
2020-07-02 08:05:41 +0200

    API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,27 @@ import { getSynonyms } from './callOpenThesaurusApi'
 import { ISynonyms } from './ISynonyms'
 import { IKeyword } from '../types'
 import { IApiResponse, IApiResponseElement } from './IApiResponse'
-import { stopwordsDe } from './stopwords-de';
-import { writeJson } from './helper';
+import { stopwordsDe } from './stopwords-de'
+import { writeJson } from './helper'
+import express = require('express')
 
 const inputSentence = 'Hallo, das ist ein Marketing text'
+const port = 8080
 
 const main = async () => {
-  const inputSentenceTokenized = tokenizeAndSanitize(inputSentence, ' ')
+  const app = express()
+
+  app.get( '/', async ( req, res ) => {
+    res.send( await getTrends(inputSentence) )
+  })
+
+  app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` )
+  })
+}
+
+const getTrends = async (input: string): Promise<IApiResponse> => {
+  const inputSentenceTokenized = tokenizeAndSanitize(input, ' ')
   const sourceKeyWords = initKeywords(inputSentenceTokenized)
   const sourceTrends = (await callAllTrends(sourceKeyWords))
 
@@ -22,12 +36,11 @@ const main = async () => {
   for (const synonym of synonyms) {
     const synonymTrends = await callAllTrends(synonym.synonyms)
     for (const syn of synonym.synonyms) {
-      syn.normalizedTrend = synonymTrends.filter(st => st.keyword === syn.keyword)[0].normalizedTrend 
+      syn.normalizedTrend = synonymTrends.filter(st => st.keyword === syn.keyword)[0].normalizedTrend
     }
   }
 
-  const apiResponse = getApiResponse(sourceTrends, synonyms)
-  writeJson('./out.json', apiResponse)
+  return getApiResponse(sourceTrends, synonyms)
 }
 
 const getApiResponse = (sourceTrends: IKeyword[], synonyms: ISynonyms[]): IApiResponse => {
